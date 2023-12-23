d6e57e809e87d74ccf22b4b437f39799fff5e757
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-12-22 22:44:16 -0300

    Cálculo de Pegada de Carbono - Funções

---
+++ pegadaDeCarbono.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+       string nome = Console.ReadLine();
+       double quilometrosPorDia = double.Parse(Console.ReadLine());
+       int horasDeEletronicos = int.Parse(Console.ReadLine());
+       int refeicoesComCarne = int.Parse(Console.ReadLine());
+
+        double pegadaDeCarbono = CalcularPegadaDeCarbono(quilometrosPorDia, horasDeEletronicos, refeicoesComCarne);
+        
+        Console.WriteLine($"{nome}, sua pegada de carbono e de {pegadaDeCarbono} toneladas de CO2 por ano.");
+
+        Console.ReadLine();
+    }
+
+    static double CalcularPegadaDeCarbono(double quilometrosPorDia, int horasDeEletronicos, int refeicoesComCarne)
+    {
+      double somaPegadaDeCaborno = (quilometrosPorDia * 365 * 0.2) 
+                                 + (horasDeEletronicos * 0.1) 
+                                 + (refeicoesComCarne * 0.5);
+      return somaPegadaDeCaborno;
+    }
+}
