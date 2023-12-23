b0b589c1086a094279e7319d4df8237e2f1bf9d5
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-12-22 22:40:40 -0300

    Catálogo de Jogos - Array e Listas

---
+++ catalogoDeJogos.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int quantidadeJogos = int.Parse(Console.ReadLine());
+
+       string[] nomesJogos = new string[quantidadeJogos];
+
+       for(int i = 0; i < quantidadeJogos; i++)
+       {
+            AdicionarJogo(i, nomesJogos);
+       }
+       
+        ExibirResumoAdicaoJogos(quantidadeJogos, nomesJogos);
+    }
+
+    static void AdicionarJogo(int indice, string[] nomes)
+    {
+        nomes[indice] = Console.ReadLine();
+    }
+
+    static void ExibirResumoAdicaoJogos(int quantidadeJogos, string[] nomes)
+    {
+        Console.WriteLine($"Foi adicionado '{quantidadeJogos}' jogos: {string.Join(", ", nomes)} ao catalogo.");
+    }
+}
